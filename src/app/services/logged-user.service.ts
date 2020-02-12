e7841d09ad5cc3232ff569d625c20ea2f74751d2
rkchaudhary4 <rkc22042000@gmail.com>
2020-02-12 12:32:51 +0530

    Upgraded angular to version 9

--- src/app/services/logged-user.service.ts
+++ src/app/services/logged-user.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
+import {
+  AngularFirestore,
+  AngularFirestoreDocument
+} from '@angular/fire/firestore';
 import { Router } from '@angular/router';
 import { BehaviorSubject, Observable, of } from 'rxjs';
 import { map, switchMap, catchError } from 'rxjs/internal/operators';
@@ -10,22 +13,30 @@ import { User } from '../models';
   providedIn: 'root'
 })
 export class LoggedUserService {
-
   currentUser: BehaviorSubject<User>;
   $logged: Observable<User>;
   isAuthenticated$: Observable<boolean>;
   role: Observable<string>;
-  public userRef = (id: string): AngularFirestoreDocument<User> => this.afs.doc('users/${id}');
+  public userRef = (id: string): AngularFirestoreDocument<User> =>
+    this.afs.doc('users/${id}');
   init = (): void => {
     this.currentUser = new BehaviorSubject<User>(null);
     this.isAuthenticated$ = this.afAuth.authState.pipe(map(res => !!res));
     this.$logged = this.afAuth.authState.pipe(
-      switchMap((user) => user ? this.userRef(user.uid).valueChanges() : of(null)),
+      switchMap(user =>
+        user ? this.userRef(user.uid).valueChanges() : of(null)
+      ),
       catchError(err => {
         return of(null);
       })
     );
-    this.$logged.subscribe((users) => this.currentUser.next(users));
+    this.$logged.subscribe(users => this.currentUser.next(users));
   }
-  constructor(private afAuth: AngularFireAuth, private afs: AngularFirestore, private router: Router) { }
+  constructor(
+    private afAuth: AngularFireAuth,
+    private afs: AngularFirestore,
+    private router: Router
+  ) {
+    this.init();
+  }
 }
